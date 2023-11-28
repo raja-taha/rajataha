e2aed8f0d20843aa5840126e8176734da5b39e87
Mr_RajaG <rajataha062@gmail.com>
2023-11-28 18:54:09 +0500

    version 0.2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import profile from "./assets/profile.jpg";
+import GitHubCalendar from "react-github-calendar";
 
 function App() {
   return (
     <div className="App">
       <h1>Muhammad Taha</h1>
       <img src={profile} width={300} alt="profile" />
+      <ul>
+        <li>
+          <a href="https://wired-wizards.vercel.app" target="_blank">
+            Wired Wizards
+          </a>
+        </li>
+        <li>
+          <a href="https://nest-videos.vercel.app" target="_blank">
+            Nest Videos
+          </a>
+        </li>
+        <li>
+          <a href="https://crypto-warrior.vercel.app" target="_blank">
+            Crypto Warrior
+          </a>
+        </li>
+        <li>
+          <a href="https://master-cart.vercel.app" target="_blank">
+            Master Cart
+          </a>
+        </li>
+      </ul>
+
+      <GitHubCalendar username="raja-taha" />
     </div>
   );
 }
