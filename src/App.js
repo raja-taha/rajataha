0ecb536e5c6fad1d0cb85907da73c08c9a248023
Mr_RajaG <rajataha062@gmail.com>
2024-01-23 15:33:25 +0500

    /

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -26,20 +25,19 @@ function App() {
   }, [ratio]);
 
   return ratio < 2.11 ? (
-    <Router>
+    <>
       <HeaderPhone menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <Routes>
-        <Route path={"/"} element={<Home ratio={ratio} />} />
-        <Route path={"/work"} element={<Work />} />
-        <Route path={"/timeline"} element={<Timeline />} />
-        <Route path={"/services"} element={<Services />} />
-        <Route path={"/testimonial"} element={<Testimonial />} />
-        <Route path={"/contact"} element={<Contact />} />
-      </Routes>
+
+      <Home ratio={ratio} />
+      <Work />
+      <Timeline />
+      <Services />
+      <Testimonial />
+      <Contact />
       <Footer />
       <Toaster />
-    </Router>
+    </>
   ) : (
     <em id="customMessage">Please Change the ratio to View!</em>
   );
